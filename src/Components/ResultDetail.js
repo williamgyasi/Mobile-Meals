0ddebeff4ca9bfefd3d265af1f7a6f9f0691b13c
William <kwabenagyasi63@gmail.com>
2021-06-09 21:15:54 +0000

    RemoteBlock

---
+++ src/Components/ResultDetail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const ResultDetial =({item})=>{
+    return(
+        <View>
+            <Text>WELCOME</Text>
+        </View>
+    )
+}
+
+export default ResultDetial;
--- src/Components/ResultsList.js
+++ src/Components/ResultsList.js
@@ -1,11 +1,19 @@
+import { Item } from 'native-base'
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text, FlatList} from 'react-native'
 
 const ResultsList=({title,results})=>{
     return(
         <View>
             <Text>{title}</Text>
-            <Text>I HAVE{results.length}</Text>
+            <FlatList 
+            data={results}
+            horizontal
+            keyExtractor={()=>results.id}
+            renderItem={({item})=>(
+                <Text>{item.name}</Text>
+            )}
+            />
         </View>
     )
 }
