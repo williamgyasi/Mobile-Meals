997891e71e163413bc6d2becc7c7ec5c5ce698c8
William <kwabenagyasi63@gmail.com>
2021-06-10 11:03:26 +0000

    Hep

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -5,19 +5,18 @@ import { createStackNavigator } from "@react-navigation/stack";
 //SCREENS
 import RandomScreen from "../Screens/Random";
 import HomeScreen from "../Screens/Home";
-import ShowResults from '../Screens/ShowResults'
+import ShowResults from "../Screens/ShowResults";
 
 const RootStack = createStackNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer
-    
-    >
-      <RootStack.Navigator initialRouteName="Homescreen"
-      screenOptions={{
-          headerShown:false,
-      }}
+    <NavigationContainer>
+      <RootStack.Navigator
+        initialRouteName="Homescreen"
+        screenOptions={{
+          headerShown: false,
+        }}
       >
         <RootStack.Screen name="Random" component={RandomScreen} />
         <RootStack.Screen name="Homescreen" component={HomeScreen} />
