197c3f58605f4380896a6da24beb3d8059f9e58b
William <kwabenagyasi63@gmail.com>
2021-07-19 15:30:07 +0000

    Firebase App

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 //NAVIGATION
-import Navigation from './src/Routes'
+import Navigation from "./src/Routes";
 
 //FIREBASE
 import * as firebase from "firebase";
 import config from "./src/Firebase/firebaseConfig";
 
 export default function App() {
-  if (!firebase.apps.length) {
-    console.log("Connected With Firebase");
-    firebase.initializeApp(config.firebaseConfig);
-  }
-  else{
-    console.log("ac")
-  }
+  // if (!firebase.apps.length) {
+  //   console.log("Connected With Firebase");
+    
+  // } else {
+  //   console.log("ac");
+  // }
 
-  return (
-    <Navigation />
-  );
+  let firebaseAppDefined = false;
+
+  // setInterval(() => {
+  //   if (!firebaseAppDefined) {
+  //     if (firebase.app()) {
+  //       console.log("Error")
+  //       firebase.initializeApp(config.firebaseConfig);
+  //       firebaseAppDefined = true;
+  //     }
+  //   }
+  // }, 100);
+
+  return <Navigation />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -1,12 +1,12 @@
 export default {
-     firebaseConfig:{
-        apiKey: "AIzaSyC4UR3ulEsckibfzHZp_zdAp4s8Ax0235I",
-        authDomain: "mobile-meals-c43b4.firebaseapp.com",
-        projectId: "mobile-meals-c43b4",
-        storageBucket: "mobile-meals-c43b4.appspot.com",
-        messagingSenderId: "979333634873",
-        appId: "1:979333634873:web:5c6019478142f9a8a226e8",
-        measurementId: "G-LGSQZR6CDN"
-      }
+    //  firebaseConfig:{
+    //     apiKey: "AIzaSyC4UR3ulEsckibfzHZp_zdAp4s8Ax0235I",
+    //     authDomain: "mobile-meals-c43b4.firebaseapp.com",
+    //     projectId: "mobile-meals-c43b4",
+    //     storageBucket: "mobile-meals-c43b4.appspot.com",
+    //     messagingSenderId: "979333634873",
+    //     appId: "1:979333634873:web:5c6019478142f9a8a226e8",
+    //     measurementId: "G-LGSQZR6CDN"
+    //   }
     
 }
